445e2a19f124d08e629c3df117b7fabb8e498d5d
tracer-ics2017 <tracer@njuics.org>
2026-05-27 15:55:14 +0800

    >  compile 2311018 wsy Linux DESKTOP-VDEQ4RT 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:55:08 up  1:32,  1 user,  load average: 0.38, 0.22, 0.13 bd86f65073af33cf41191199874d484a7bd887e5

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,11 +5,14 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
+  TK_NUM, TK_HEX, TK_REG, TK_DEREF, TK_NEG, TK_NEQ, TK_AND, TK_OR
 
 };
 
@@ -24,7 +27,20 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"==", TK_EQ},         // equal
+  {"!=", TK_NEQ},       // not equal
+  {"&&", TK_AND},        // logical and
+  {"\\|\\|", TK_OR},    // logical or
+  {"!", '!'},            // logical not
+  {"\\*", '*'},          // multiply or dereference
+  {"/", '/'},            // divide
+  {"-", '-'},            // minus or negative
+  {"\\(", '('},          // left paren
+  {"\\)", ')'},          // right paren
+  {"0x[0-9a-fA-F]+", TK_HEX},  // hexadecimal number
+  {"[0-9]+", TK_NUM},          // decimal number
+  {"\\$[a-zA-Z]+", TK_REG},    // register
+
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -51,6 +67,7 @@ void init_regex() {
 typedef struct token {
   int type;
   char str[32];
+  int priority;
 } Token;
 
 Token tokens[32];
@@ -74,13 +91,106 @@ static bool make_token(char *e) {
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
+        /* Record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
 
+        if (nr_token >= 32) {
+          printf("Too many tokens!\n");
+          return false;
+        }
+
         switch (rules[i].token_type) {
-          default: TODO();
+          case TK_NOTYPE:
+            break;
+          case TK_NUM:
+          case TK_HEX:
+          case TK_REG:
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            tokens[nr_token].type = rules[i].token_type;
+            tokens[nr_token].priority = 0;
+            nr_token++;
+            break;
+          case '*':
+          case '/':
+            /* Check if * is dereference or multiply */
+            if (rules[i].token_type == '*') {
+              if (nr_token == 0 || tokens[nr_token - 1].type == '(' ||
+                  tokens[nr_token - 1].type == '+' || tokens[nr_token - 1].type == '-' ||
+                  tokens[nr_token - 1].type == '*' || tokens[nr_token - 1].type == '/' ||
+                  tokens[nr_token - 1].type == TK_DEREF || tokens[nr_token - 1].type == TK_NEG) {
+                tokens[nr_token].type = TK_DEREF;
+                tokens[nr_token].priority = 7;
+              } else {
+                tokens[nr_token].type = '*';
+                tokens[nr_token].priority = 4;
+              }
+            } else if (rules[i].token_type == '/') {
+              tokens[nr_token].type = '/';
+              tokens[nr_token].priority = 4;
+            }
+            nr_token++;
+            break;
+          case '-':
+            /* Check if - is negative or minus */
+            if (nr_token == 0 || tokens[nr_token - 1].type == '(' ||
+                tokens[nr_token - 1].type == '+' || tokens[nr_token - 1].type == '-' ||
+                tokens[nr_token - 1].type == '*' || tokens[nr_token - 1].type == '/' ||
+                tokens[nr_token - 1].type == TK_DEREF || tokens[nr_token - 1].type == TK_NEG) {
+              tokens[nr_token].type = TK_NEG;
+              tokens[nr_token].priority = 7;
+            } else {
+              tokens[nr_token].type = '-';
+              tokens[nr_token].priority = 3;
+            }
+            nr_token++;
+            break;
+          case '+':
+            tokens[nr_token].type = '+';
+            tokens[nr_token].priority = 3;
+            nr_token++;
+            break;
+          case '(':
+            tokens[nr_token].type = '(';
+            tokens[nr_token].priority = 0;
+            nr_token++;
+            break;
+          case ')':
+            tokens[nr_token].type = ')';
+            tokens[nr_token].priority = 0;
+            nr_token++;
+            break;
+          case TK_EQ:
+            tokens[nr_token].type = TK_EQ;
+            tokens[nr_token].priority = 2;
+            nr_token++;
+            break;
+          case TK_NEQ:
+            tokens[nr_token].type = TK_NEQ;
+            tokens[nr_token].priority = 2;
+            nr_token++;
+            break;
+          case TK_AND:
+            tokens[nr_token].type = TK_AND;
+            tokens[nr_token].priority = 1;
+            nr_token++;
+            break;
+          case TK_OR:
+            tokens[nr_token].type = TK_OR;
+            tokens[nr_token].priority = 0;
+            nr_token++;
+            break;
+          case '!':
+            tokens[nr_token].type = '!';
+            tokens[nr_token].priority = 7;
+            nr_token++;
+            break;
+          default:
+            tokens[nr_token].type = rules[i].token_type;
+            tokens[nr_token].priority = 0;
+            nr_token++;
+            break;
         }
 
         break;
@@ -96,14 +206,155 @@ static bool make_token(char *e) {
   return true;
 }
 
+/* Check if parentheses are balanced */
+static bool check_parentheses(int p, int q) {
+  int i, balance = 0;
+  for (i = p; i <= q; i++) {
+    if (tokens[i].type == '(') balance++;
+    else if (tokens[i].type == ')') balance--;
+    if (balance < 0) return false;
+  }
+  return balance == 0;
+}
+
+/* Find the dominant operator */
+static int find_dominant_operator(int p, int q) {
+  int i;
+  int pos = -1;
+  int min_priority = 10;
+  int balance = 0;
+
+  for (i = p; i <= q; i++) {
+    if (tokens[i].type == '(') {
+      balance++;
+      continue;
+    }
+    if (tokens[i].type == ')') {
+      balance--;
+      continue;
+    }
+    if (balance != 0) continue;  /* Inside parentheses */
+
+    if (tokens[i].type == '+' || tokens[i].type == '-' ||
+        tokens[i].type == '*' || tokens[i].type == '/' ||
+        tokens[i].type == TK_EQ || tokens[i].type == TK_NEQ ||
+        tokens[i].type == TK_AND || tokens[i].type == TK_OR) {
+      if (tokens[i].priority <= min_priority) {
+        min_priority = tokens[i].priority;
+        pos = i;
+      }
+    }
+  }
+
+  return pos;
+}
+
+/* Evaluate expression recursively */
+static uint32_t eval(int p, int q, bool *success) {
+  if (p > q) {
+    *success = false;
+    return 0;
+  }
+
+  if (p == q) {
+    /* Single token */
+    switch (tokens[p].type) {
+      case TK_NUM:
+        *success = true;
+        return atoi(tokens[p].str);
+      case TK_HEX:
+        *success = true;
+        return (uint32_t)strtoul(tokens[p].str, NULL, 16);
+      case TK_REG: {
+        int i;
+        const char *reg_name = tokens[p].str + 1;  /* Skip '$' */
+        for (i = 0; i < 8; i++) {
+          if (strcmp(reg_name, regsl[i]) == 0) {
+            *success = true;
+            return cpu.gpr[i]._32;
+          }
+        }
+        if (strcmp(reg_name, "eip") == 0) {
+          *success = true;
+          return cpu.eip;
+        }
+        *success = false;
+        return 0;
+      }
+      default:
+        *success = false;
+        return 0;
+    }
+  }
+
+  if (check_parentheses(p, q) == true) {
+    if (tokens[p].type == '(' && tokens[q].type == ')') {
+      return eval(p + 1, q - 1, success);
+    }
+  } else {
+    *success = false;
+    return 0;
+  }
+
+  /* Find dominant operator */
+  int op = find_dominant_operator(p, q);
+
+  if (op == -1) {
+    /* No operator found, must be a unary operator */
+    if (tokens[p].type == TK_NEG) {
+      uint32_t val = eval(p + 1, q, success);
+      if (!*success) return 0;
+      *success = true;
+      return -val;
+    }
+    if (tokens[p].type == TK_DEREF) {
+      uint32_t addr = eval(p + 1, q, success);
+      if (!*success) return 0;
+      *success = true;
+      return vaddr_read(addr, 4);
+    }
+    if (tokens[p].type == '!') {
+      uint32_t val = eval(p + 1, q, success);
+      if (!*success) return 0;
+      *success = true;
+      return !val;
+    }
+    *success = false;
+    return 0;
+  }
+
+  uint32_t val1 = eval(p, op - 1, success);
+  if (!*success) return 0;
+  uint32_t val2 = eval(op + 1, q, success);
+  if (!*success) return 0;
+
+  switch (tokens[op].type) {
+    case '+': *success = true; return val1 + val2;
+    case '-': *success = true; return val1 - val2;
+    case '*': *success = true; return val1 * val2;
+    case '/':
+      if (val2 == 0) {
+        *success = false;
+        return 0;
+      }
+      *success = true;
+      return val1 / val2;
+    case TK_EQ: *success = true; return val1 == val2;
+    case TK_NEQ: *success = true; return val1 != val2;
+    case TK_AND: *success = true; return val1 && val2;
+    case TK_OR: *success = true; return val1 || val2;
+    default:
+      *success = false;
+      return 0;
+  }
+}
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  /* Evaluate the expression */
+  return eval(0, nr_token - 1, success);
 }
--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,136 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  char *arg = strtok(NULL, " ");
+  if (arg == NULL) {
+    cpu_exec(1);
+  } else {
+    int n = atoi(arg);
+    if (n <= 0) {
+      printf("Invalid number: %s\n", arg);
+      return 0;
+    }
+    cpu_exec(n);
+  }
+  return 0;
+}
+
+static int cmd_info(char *args) {
+  char *arg = strtok(NULL, " ");
+  if (arg == NULL) {
+    printf("Usage: info r | info w\n");
+    return 0;
+  }
+
+  if (strcmp(arg, "r") == 0) {
+    int i;
+    for (i = 0; i < 8; i++) {
+      printf("%-3s\t0x%08x\t%d\n", regsl[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
+    }
+    printf("eip\t0x%08x\t%d\n", cpu.eip, cpu.eip);
+  } else if (strcmp(arg, "w") == 0) {
+    // TODO: implement info watchpoints
+    printf("TODO: implement info watchpoints\n");
+  } else {
+    printf("Unknown argument: %s\n", arg);
+  }
+  return 0;
+}
+
+static int cmd_p(char *args) {
+  if (args == NULL || *args == '\0') {
+    printf("Usage: p EXPR\n");
+    return 0;
+  }
+
+  bool success;
+  uint32_t result = expr(args, &success);
+
+  if (success) {
+    printf("0x%08x (%u)\n", result, result);
+  } else {
+    printf("Invalid expression\n");
+  }
+
+  return 0;
+}
+
+static int cmd_x(char *args) {
+  char *n_str = strtok(NULL, " ");
+  char *expr_str = strtok(NULL, " ");
+
+  if (n_str == NULL || expr_str == NULL) {
+    printf("Usage: x N EXPR\n");
+    return 0;
+  }
+
+  int n = atoi(n_str);
+  if (n <= 0) {
+    printf("Invalid number: %s\n", n_str);
+    return 0;
+  }
+
+  bool success;
+  vaddr_t addr = expr(expr_str, &success);
+
+  if (!success) {
+    printf("Invalid expression\n");
+    return 0;
+  }
+
+  printf("0x%08x: ", addr);
+  int i;
+  for (i = 0; i < n; i++) {
+    if (i > 0 && i % 4 == 0) {
+      printf("\n0x%08x: ", addr + i * 4);
+    }
+    uint32_t data = vaddr_read(addr + i * 4, 4);
+    printf("%08x ", data);
+  }
+  printf("\n");
+
+  return 0;
+}
+
+static int cmd_w(char *args) {
+  if (args == NULL || *args == '\0') {
+    printf("Usage: w EXPR\n");
+    return 0;
+  }
+
+  bool success;
+  uint32_t result = expr(args, &success);
+
+  if (!success) {
+    printf("Invalid expression\n");
+    return 0;
+  }
+
+  // TODO: implement watchpoint
+  printf("TODO: set watchpoint at 0x%08x\n", result);
+
+  return 0;
+}
+
+static int cmd_d(char *args) {
+  if (args == NULL || *args == '\0') {
+    printf("Usage: d N\n");
+    return 0;
+  }
+
+  int n = atoi(args);
+  if (n <= 0) {
+    printf("Invalid number: %s\n", args);
+    return 0;
+  }
+
+  // TODO: implement delete watchpoint
+  printf("TODO: delete watchpoint %d\n", n);
+
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,8 +176,12 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
-  /* TODO: Add more commands */
+  { "si", "Single step execution", cmd_si },
+  { "info", "Display information about registers or watchpoints", cmd_info },
+  { "p", "Evaluate expression", cmd_p },
+  { "x", "Examine memory", cmd_x },
+  { "w", "Set watchpoint", cmd_w },
+  { "d", "Delete watchpoint", cmd_d },
 
 };
 
