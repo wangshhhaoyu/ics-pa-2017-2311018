6a09a80b685db8b74180c7974c85dff98bfab606
tracer-ics2017 <tracer@njuics.org>
2026-05-28 21:43:46 +0800

    >  compile 2311018 王邵与 Linux DESKTOP-VDEQ4RT 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:43:40 up 12:58,  1 user,  load average: 0.65, 1.18, 1.07 ef4ba961b5c8c5abb5b3de51e62347f7b55b860

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,7 +25,7 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  rtl_li(&t0, cpu.eip + 5);
+  rtl_li(&t0, *eip);
   rtl_push(&t0);
   decoding.is_jmp = 1;
 
