2c22bf0fa27a1cf1cb278b7cdfa0c29b6eccf547
tracer-ics2017 <tracer@njuics.org>
2026-05-29 16:46:08 +0800

    >  compile 2311018 王邵与 Linux DESKTOP-VDEQ4RT 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:46:05 up 28 min,  1 user,  load average: 0.36, 0.37, 0.28 e0b7a9d829b1f18b4715e7b4167a1c271bd7d82

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -35,6 +35,10 @@ void _draw_sync() {
 
 int _read_key() {
     uint32_t key = inl(0x60);         // 改为读取 4 字节
+    static int cnt = 0;
+    if (cnt++ < 10) {  // 只打印前10次，避免刷屏
+        printf("_read_key: 0x%x\n", key);
+    }
     if ((key & 0xff) == 0 || (key & 0xff) == 0xe0) return _KEY_NONE; // 可选：过滤无效键值
     return key;                       // 直接返回 NEMU 队列中的值（已包含按下/释放标志）
 }
