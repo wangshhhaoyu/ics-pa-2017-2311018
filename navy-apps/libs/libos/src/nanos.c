b8cdbb4fa284a73aa24dc8906089969c77d0dfb0
tracer-ics2017 <tracer@njuics.org>
2026-05-31 18:17:50 +0800

    >  run 2311018 王邵与 Linux DESKTOP-VDEQ4RT 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:17:48 up 13:38,  1 user,  load average: 1.06, 0.71, 0.50 6c0ca21a76e509005cbb5d2846e5f118abda7ab6

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -22,39 +22,40 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  // _exit(SYS_open);
   return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count){
+  // _exit(SYS_write);
   return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
 
 void *_sbrk(intptr_t increment){
-  extern int end;
-  static uintptr_t probreak = (uintptr_t)&end;
-  uintptr_t probreak_new = probreak + increment;
-  int r = _syscall_(SYS_brk, probreak_new, 0, 0);
-  if(r == 0) {
-    uintptr_t temp = probreak;
-    probreak = probreak_new;
-    return (void*)temp;
+  extern char end;
+  static char *cur_brk = NULL;
+  if (cur_brk == NULL) {
+    cur_brk = &end;
   }
+
+  char *old_brk = cur_brk;
+  char *new_brk = cur_brk + increment;
+  if (_syscall_(SYS_brk, (uintptr_t)new_brk, 0, 0) == 0) {
+    cur_brk = new_brk;
+    return (void *)old_brk;
+  }
+
   return (void *)-1;
 }
 
 int _read(int fd, void *buf, size_t count) {
-  // _exit(SYS_read);
   return _syscall_(SYS_read, fd, (uintptr_t)buf, count);
 }
 
 int _close(int fd) {
-  // _exit(SYS_close);
   return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  // _exit(SYS_lseek);
   return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,14 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
+  char buf[128];
   while (1) {
     j ++;
     if (j == 10000) {
-      printf("Hello World for the %dth time\n", i ++);
+      sprintf(buf, "Hello World for the %dth time\n", i ++);
+      write(1, buf, strlen(buf));
       j = 0;
     }
   }
